3a8f02bb8200bc8e581b7db8f980e069e033d125
PranayRewane <49708468+PranayRewane@users.noreply.github.com>
2021-05-11 22:44:43 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8352e828130ed9575204d6890dea3fbe.js"
+  "/precache-manifest.67a324b1ef76dea2237837d6a2340930.js"
 );
 
 self.addEventListener('message', (event) => {
